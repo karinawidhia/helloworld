b95a14a09df14783088167f98481cf6b317dcd47
Karina Widhia Nugrahini <karinwidhia@gmail.com>
2017-01-05 19:22:23 +0700

    commit1

---
+++ helloworld/Controllers/HelloController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace helloworld.Controllers
+{
+    public class HelloController : Controller
+    {
+        //
+        // GET: /Hello/
+        public ActionResult HelloWorld()
+        {
+            return View();
+        }
+	}
+}
